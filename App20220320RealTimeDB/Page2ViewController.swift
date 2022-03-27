01188f94d26b5b559c5c9690df6dac2a0506a443
Huang Grace <huang.yahui.grace@gmail.com>
2022-03-27 16:16:16 +0800

    read doctor from

--- App20220320RealTimeDB/Page2ViewController.swift
+++ App20220320RealTimeDB/Page2ViewController.swift
@@ -12,19 +12,26 @@ class Page2ViewController: UIViewController {
     // 設定資料庫reference
     var dbRef:DatabaseReference!
     var nickname = ""
+    var subjectArray: [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.title = "Welcome: \(nickname)"
         
-        dbRef = Database.database().reference()
+        dbRef = Database.database().reference().child("for/subs")
         dbRef.observeSingleEvent(of: .value) { dataSnapshot in
-            print(dataSnapshot)
+//            print(dataSnapshot)
+            self.subjectArray.removeAll()
+            
             for item in dataSnapshot.children {
-                print("=========")
-                print(item)
+                if let mySubject = item as? DataSnapshot {
+                    let subject = mySubject.childSnapshot(forPath: "subject").value as? String ?? ""
+                    self.subjectArray.append(subject)
+                }
             }
+            
+            print(self.subjectArray)
         }
     }
 }
