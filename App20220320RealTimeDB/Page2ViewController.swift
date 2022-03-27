2121700e1d42c3f8d270c6a00a8ac18ae4327149
Huang Grace <huang.yahui.grace@gmail.com>
2022-03-27 16:23:34 +0800

    將realtime db data 顯示到TableView

--- App20220320RealTimeDB/Page2ViewController.swift
+++ App20220320RealTimeDB/Page2ViewController.swift
@@ -8,20 +8,25 @@
 import UIKit
 import Firebase
 
-class Page2ViewController: UIViewController {
+class Page2ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     // 設定資料庫reference
     var dbRef:DatabaseReference!
     var nickname = ""
     var subjectArray: [String] = []
     
+    @IBOutlet weak var myTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.title = "Welcome: \(nickname)"
         
+        // MARK: TableView
+        myTableView.delegate = self
+        myTableView.dataSource = self
+        
+        // MARK: RealTime DataBase
         dbRef = Database.database().reference().child("for/subs")
         dbRef.observeSingleEvent(of: .value) { dataSnapshot in
-//            print(dataSnapshot)
             self.subjectArray.removeAll()
             
             for item in dataSnapshot.children {
@@ -32,6 +37,21 @@ class Page2ViewController: UIViewController {
             }
             
             print(self.subjectArray)
+            
+            // 讀完資料要重Load TableView
+            self.myTableView.reloadData()
         }
     }
+    
+    // MARK: TableView
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return subjectArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = subjectArray[indexPath.row]
+        return cell
+    }
+    
 }
